f2e9c1733a81f279cc6cfbf8048ae0107d7d776b
Zeke Gomez <[zzekegomez09@gmail.com]>
2020-01-31 18:26:41 -0600

    Finished at step 10

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+// export class AppComponent {
+//   title = 'NG421wk1day1ToDoApp';
+// }
+
 export class AppComponent {
-  title = 'NG421wk1day1ToDoApp';
+  title = 'Todos';
+  todoList: any[];
+
+  ngOnInit() {
+  // guessing I dont hav angular cli installed
+  this.todoList = [
+    {title:'Todo', isDone:false}
+  ];
 }
+
+deleteTodo(todo:any) {
+  const index = this.todoList.findIndex(todoItem => todoItem === todo);
+  this.todoList.splice(index,1);
+}
+}
+
